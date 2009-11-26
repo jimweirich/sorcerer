a8cca15e0c94f43ced9216abae5b306d4f297793
Jim Weirich <jim.weirich@gmail.com>
2009-11-25 23:42:16 -0500

    Added top level invocation for subexpressions

--- lib/sorcerer.rb
+++ lib/sorcerer.rb
@@ -1,7 +1,12 @@
 module Sorcerer
   # Generate the source code for teh given Ripper S-Expression.
-  def Sorcerer.source(sexp, debug=false)
+  def self.source(sexp, debug=false)
     Sorcerer::Resource.new(sexp, debug).source
+  end
+
+  # Generate a list of interesting subexpressions for sexp.
+  def self.subexpressions(sexp)
+    Sorcerer::Subexpression.new(sexp)
   end
 end
 
