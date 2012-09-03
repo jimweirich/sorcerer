f8a2208854b7a20ef6933ca9cacabf9c394e1a5f
Jim Weirich <jim.weirich@gmail.com>
2012-09-03 14:24:23 -0400

    Fix version typo

--- lib/sorcerer/version.rb
+++ lib/sorcerer/version.rb
@@ -1,8 +1,8 @@
 module Sorcerer
   VERSION_NUMBERS = [
-    VERSION_MAJOR = 0
-    VERSION_MINOR = 3
-    VERSION_BUILD = 3
+    VERSION_MAJOR = 0,
+    VERSION_MINOR = 3,
+    VERSION_BUILD = 3,
   ]
 
   VERSION = VERSION_NUMBERS.join('.')
